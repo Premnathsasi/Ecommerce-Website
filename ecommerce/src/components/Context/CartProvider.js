7c41c3ea9a535adc0c44ffd69da8e83453b6be42
Premnathsasi <premnathsasi4@gmail.com>
2023-03-17 14:18:16 +0530

    task 8

--- ecommerce/src/components/Context/CartProvider.js
+++ ecommerce/src/components/Context/CartProvider.js
@@ -1,56 +1,60 @@
-import React, {useReducer} from "react";
-import CartContext from './CartContext';
+import React, { useReducer } from "react";
+import CartContext from "./CartContext";
 
 const defaultState = {
-    items:[],
-}
-
-const cartReducer = (state, action) => {
-        if (action.type === 'ADD') {
-            const existingCartItem = state.items.find(item =>  {if (item.id === action.item.id) return true;
-            return false
-        } );
-            let updatedItems;
-           if (existingCartItem) {
-            
-             updatedItems =  [...state.items.filter(el => el.id !== action.item.id), action.item]
-           }else {
-             updatedItems = state.items.concat(action.item);};
-            return {
-                items: updatedItems,
-            };
-        }
-
-        if (action.type === 'REMOVE') {
-            const updatedItems = state.items.filter(item => item.id !== action.id);
-            return {
-                items: updatedItems
-            }
-        }
+  items: [],
 };
 
-const CartProvider = props => {
-    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultState);
-
-    const addItemToCartHandler = item => {
-        dispatchCartAction({type:'ADD', item: item})
-    };
-    
-    const removeItemHandler = id => {
-        dispatchCartAction({type: 'REMOVE', id: id})
-    };
-    
-    const cartContext = {
-        items:cartState.items,
-        addItem:addItemToCartHandler,
-        removeItem: removeItemHandler
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const existingCartItem = state.items.find((item) => {
+      if (item.id === action.item.id) return true;
+      return false;
+    });
+    let updatedItems;
+    if (existingCartItem) {
+      updatedItems = [
+        ...state.items.filter((el) => el.id !== action.item.id),
+        action.item,
+      ];
+    } else {
+      updatedItems = state.items.concat(action.item);
     }
+    return {
+      items: updatedItems,
+    };
+  }
 
-    return (
-        <CartContext.Provider value={cartContext}>
-            {props.children}
-        </CartContext.Provider>
-    )
+  if (action.type === "REMOVE") {
+    const updatedItems = state.items.filter((item) => item.id !== action.id);
+    return {
+      items: updatedItems,
+    };
+  }
+};
+
+const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultState);
+
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD", item: item });
+  };
+
+  const removeItemHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
+
+  const cartContext = {
+    items: cartState.items,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemHandler,
+  };
+
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
 };
 
 export default CartProvider;
--- http-request/src/App.js
+++ http-request/src/App.js
@@ -1,26 +1,60 @@
 import React, { useState } from "react";
-
+import LoadingSpinner from "./components/LoadingSpinner";
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [isLoading, setisLoading] = useState(false)
+  const [isLoading, setisLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [retry, setRetry] = useState(true);
+
+  const cancelRetry = () => {
+    setRetry(false);
+  };
+  const retryFetch = () => {
+    return new Promise((res)=>setTimeout(res,5000))
+  }
+
   async function fetchMovieHandler() {
-    
-    setisLoading(true)
-    const response = await fetch("https://swapi.dev/api/films");
-    const data = await response.json();
-    const transformedData = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setMovies(transformedData);
-    setisLoading(false)
+    setisLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch("https://swapi.dev/api/film");
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+
+      const data = await response.json();
+      const transformedData = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedData);
+    } catch (error) {
+      setError(error.message);
+      if (retry) retryFetch().then(fetchMovieHandler)
+    }
+    setisLoading(false);
+  }
+
+  let content = <p>No movies found</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <LoadingSpinner />;
   }
 
   return (
@@ -28,12 +62,9 @@ function App() {
       <section>
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
-      <section>
-       {!isLoading && movies.length >0 && <MoviesList movies={movies} /> }
-       {!isLoading && movies.length === 0 && <p>No movies found!</p>}
-       {isLoading && <p>Loading...</p>}
-
-      </section>
+      <section>{content}
+       {error && retry && <div><button onClick={cancelRetry}>Cancel Retrying</button></div>}</section>
+     
     </React.Fragment>
   );
 }
