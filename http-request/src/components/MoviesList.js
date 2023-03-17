980e02bd65b304736aca49d8aa19fb9b45f20976
Premnathsasi <premnathsasi4@gmail.com>
2023-03-17 17:28:37 +0530

    task 11

--- http-request/src/components/MoviesList.js
+++ http-request/src/components/MoviesList.js
@@ -5,13 +5,15 @@ import classes from './MoviesList.module.css';
 
 const MovieList = (props) => {
   return (
-    <ul className={classes['movies-list']}>
+    <ul  className={classes['movies-list']}>
       {props.movies.map((movie) => (
         <Movie
+        id={movie.id}
           key={movie.id}
           title={movie.title}
           releaseDate={movie.releaseDate}
           openingText={movie.openingText}
+          onClick={props.onClick}
         />
       ))}
     </ul>
