7bfcb6c77d08ad7bc39d61d12920cb931245c0c8
Premnathsasi <premnathsasi4@gmail.com>
2023-03-18 15:07:36 +0530

    task 16 login added

--- authentication/src/components/Auth/AuthForm.js
+++ authentication/src/components/Auth/AuthForm.js
@@ -4,41 +4,45 @@ import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
-  const [isLoading, setisLoading] = useState(false)
+  const [isLoading, setisLoading] = useState(false);
   const emailInput = useRef();
   const passwordInput = useRef();
-  const submitHandler = (e) => {
-   
+  const submitHandler = async function(e) {
     e.preventDefault();
     const enteredMail = emailInput.current.value;
     const enteredPassword = passwordInput.current.value;
-    setisLoading(true)
+    setisLoading(true);
+
+    try {
+      let url;
     if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBbPUzHTIrzC1b_7sfYfKpf8FaR_yyocBc";
     } else {
-      fetch(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBbPUzHTIrzC1b_7sfYfKpf8FaR_yyocBc",
-        {
-          method: "POST",
-          body: JSON.stringify({
-            email: enteredMail,
-            password: enteredPassword,
-            returnSecureToken: true,
-          }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      ).then((res) => {
-        setisLoading(false)
-        if (res.ok) {
-        } else {
-          return res.json().then((data) => {
-            alert(data.error.message);  
-          });
-        }
-      });
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBbPUzHTIrzC1b_7sfYfKpf8FaR_yyocBc";
     }
-    
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredMail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    setisLoading(false)
+    if (!response.ok) {
+      let errMsg = 'Authentication Failed';
+          throw new Error(errMsg)
+    }
+    const data = await response.json();
+    console.log(data)
+    } catch(error) {
+      alert(error.message)
+    }
   };
 
   const switchAuthModeHandler = () => {
@@ -58,8 +62,10 @@ const AuthForm = () => {
           <input type="password" id="password" required ref={passwordInput} />
         </div>
         <div className={classes.actions}>
-        {!isLoading && <button>{isLogin? 'Login' : 'Create Account'}</button>}
-        {isLoading && <p>Sending Request</p>}
+          {!isLoading && (
+            <button>{isLogin ? "Login" : "Create Account"}</button>
+          )}
+          {isLoading && <p>Sending Request</p>}
 
           <button
             type="button"
@@ -72,8 +78,6 @@ const AuthForm = () => {
       </form>
     </section>
   );
-  
 };
-
 
 export default AuthForm;
